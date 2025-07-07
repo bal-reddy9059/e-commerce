f9786d74028232ec48d8b223e06f81e3aa35a2d6
Devarla Bal Reddy <balreddy211228@gmail.com>
2025-07-06 20:13:17 -0700

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,4 @@
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from './App';
-// import './index.css';
-// import ShopContextProvider from './Context/ShopContext';
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//    <BrowserRouter>
-//       <ShopContextProvider>
-//         <App />
-//        {/* // <ProductManager /> */}
-//       </ShopContextProvider>
-//   </BrowserRouter>
-//  </React.StrictMode>
-// );
-
-
-// src/main.jsx
-
-
-// src/main.jsx
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -34,7 +6,7 @@ import App from './App';
 import ShopContextProvider from './Context/ShopContext'; // ✅ Import your context provider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter >
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
