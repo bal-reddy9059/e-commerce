5de32ceb8f59402d7db1e0225af672fd0c89e776
Devarla Bal Reddy <balreddy211228@gmail.com>
2025-10-29 18:27:48 +0530

    Added responsive navbar and updated cart layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import ShopContextProvider from './Context/ShopContext'; 
+import ShopContextProvider from './Context/ShopContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Deploy1">
-  <ShopContextProvider>
-    <App />
-  </ShopContextProvider>
-</BrowserRouter>
-
+  <BrowserRouter
+    future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+  >
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
 );
